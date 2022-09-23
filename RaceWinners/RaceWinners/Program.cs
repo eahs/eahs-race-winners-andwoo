f195ef95f78842ec5add8bcc6c9c6406902255cf
Andrew Xia <xiathschool@gmail.com>
2022-09-23 12:26:45 -0400

    Completed project

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -12,13 +12,25 @@ namespace RaceWinners
             // Asynchronously retrieve the group (class) data
             var data = await ds.GetGroupRanksAsync();
 
+            double fraction = 0.8;
+            int count = 0;
+
             for (int i = 0; i < data.Count; i++)
             {
+                count += data[i].Ranks.Count;
+            }
+
+                for (int i = 0; i < data.Count; i++)
+            {
                 double points = 0;
-                
-                for (int rank: data[i].Ranks)
-                
-                Console.WriteLine($"{data[i].Name} - [{ranks}]");
+
+                var ranks = data[i].Ranks;
+
+                foreach (var rank in ranks)
+                {
+                    points += 100 * Math.Pow(fraction, rank - 1);
+                }
+                Console.WriteLine($"{data[i].Name} - {Math.Round(points, 2)}");
             }
         }
     }
